cdabf9e3ad61f1040d6647efce10c13e284f71f3
Sunny CHEN <sunny.chen@st.com>
2026-05-19 11:24:28 +0800

    [rot ecc384] Test with default config for TZ App (img num 2, overwrite only)

--- Projects/NUCLEO-U385RG-Q/ROT_Provisioning/OEMiROT_384/ob_flash_programming.bat
+++ Projects/NUCLEO-U385RG-Q/ROT_Provisioning/OEMiROT_384/ob_flash_programming.bat
@@ -20,27 +20,27 @@ set bootpath=OEMiRoT
 
 :: Data updated with the postbuild of OEMiROT-Boot
 set sec1_start=0x0
-set sec1_end=0x32
+set sec1_end=0x20
 set sec2_start=0x7F
 set sec2_end=0x0
 set wrp1a_start=0x4
-set wrp1a_end=0x19
+set wrp1a_end=0x17
 set wrp2a_start=0x7A
 set wrp2a_end=0x7F
 set wrp2b_start=
 set wrp2b_end=
 set hdp1_start=
-set hdp1_end=0x29
+set hdp1_end=0x17
 set hdp2_start=0x7f
-set hdp2_end=0x29
+set hdp2_end=0x17
 set boot_lck=
 set secbootadd0=0x1800C0
 set flashsectnbr=0x7F
 
-set slot0=0xC02A000
-set slot1=0xC033000
-set slot2=0xC065000
-set slot3=0xC06E000
+set slot0=0xC018000
+set slot1=0xC021000
+set slot2=0xC053000
+set slot3=0xC05C000
 set slot4=0x0
 set slot5=0x0
 set slot6=0x0
